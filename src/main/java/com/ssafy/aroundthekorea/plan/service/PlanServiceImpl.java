a4158dceee627a811dd0a349bcb0c102b25ffe7e
허동민 <gjehdals456@gmail.com>
2024-05-20 09:42:19 +0900

    Fix : TravelPlan orderIndex error in DB

--- src/main/java/com/ssafy/aroundthekorea/plan/service/PlanServiceImpl.java
+++ src/main/java/com/ssafy/aroundthekorea/plan/service/PlanServiceImpl.java
@@ -1,6 +1,8 @@
 package com.ssafy.aroundthekorea.plan.service;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -28,13 +30,23 @@ public class PlanServiceImpl implements PlanService {
 
 	@Override
 	public void modifyOrder(List<TravelPlanOrderRequest> orderList, Integer planId) {
-		for (int i = 0; i < orderList.size(); i++) {
-			TravelPlanOrderRequest travelPlanOrderReqeust = orderList.get(i);
-			TravelPlan travelPlan = travelPlanRepository
-					.findByAttractionInfoId(travelPlanOrderReqeust.getAttractionInfoId());
-			travelPlan.setOrderIndex(travelPlanOrderReqeust.getOrderIndex());
-			travelPlanRepository.save(travelPlan);
-		}
+		 // planId에 맞는 모든 TravelPlan 객체를 가져옴
+        List<TravelPlan> travelPlans = travelPlanRepository.findAllByPlanId(planId);
+
+        // orderList를 Map으로 변환하여 attractionInfoId를 키로 매핑
+        Map<Integer, Integer> orderMap = orderList.stream()
+                .collect(Collectors.toMap(TravelPlanOrderRequest::getAttractionInfoId, TravelPlanOrderRequest::getOrderIndex));
+
+        // TravelPlan 객체들의 orderIndex 업데이트
+        for (TravelPlan travelPlan : travelPlans) {
+            Integer newOrderIndex = orderMap.get(travelPlan.getAttractionInfoId());
+            if (newOrderIndex != null) {
+                travelPlan.setOrderIndex(newOrderIndex);
+            }
+        }
+
+        // 업데이트된 TravelPlan 객체들을 저장
+        travelPlanRepository.saveAll(travelPlans);
 	}
 
 	@Override
